13db763c7e86d029c10d3f5d79e119988a16e446
Alexandr Kozlenkov <sah4ez32@gmail.com>
2022-07-06 12:54:42 +0300

    Added example js client

--- pkg/interfaces/tg.go
+++ pkg/interfaces/tg.go
@@ -3,6 +3,7 @@
 // @tg description=`Simple summator`
 // @tg servers=`http://localhost:9000`
 //go:generate tg client -go --services . --outPath ../clients/user
+//go:generate tg client -js --services . --outPath ../../js
 //go:generate tg transport --services . --out ../transport --outSwagger ../../api/user-openapi.yaml
 //go:generate goimports -l -w ../transport ../clients/user
 package interfaces
